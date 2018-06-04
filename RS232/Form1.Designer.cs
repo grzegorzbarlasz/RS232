ba0e3f2296cf4c73b635fb3f2f3e5026fbb63f39
grzegorzbarlasz <grzesiubyd@gmail.com>
2018-06-04 18:40:26 +0200

    simple application to communication by RS

---
+++ RS232/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace RS232
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxComPorts = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaudrate = new System.Windows.Forms.ComboBox();
+            this.comboBoxDataBits = new System.Windows.Forms.ComboBox();
+            this.comboBoxParity = new System.Windows.Forms.ComboBox();
+            this.comboBoxStopBits = new System.Windows.Forms.ComboBox();
+            this.comboBoxHandshake = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.richTextBoxIncomingData = new System.Windows.Forms.RichTextBox();
+            this.labelConnected = new System.Windows.Forms.Label();
+            this.DLRadio = new System.Windows.Forms.RadioButton();
+            this.PHYRadio = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // comboBoxComPorts
+            // 
+            this.comboBoxComPorts.FormattingEnabled = true;
+            this.comboBoxComPorts.Location = new System.Drawing.Point(13, 13);
+            this.comboBoxComPorts.Name = "comboBoxComPorts";
+            this.comboBoxComPorts.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxComPorts.TabIndex = 0;
+            this.comboBoxComPorts.Text = "COM";
+            // 
+            // comboBoxBaudrate
+            // 
+            this.comboBoxBaudrate.FormattingEnabled = true;
+            this.comboBoxBaudrate.Location = new System.Drawing.Point(109, 13);
+            this.comboBoxBaudrate.Name = "comboBoxBaudrate";
+            this.comboBoxBaudrate.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxBaudrate.TabIndex = 1;
+            this.comboBoxBaudrate.Text = "BAUDRATE";
+            // 
+            // comboBoxDataBits
+            // 
+            this.comboBoxDataBits.FormattingEnabled = true;
+            this.comboBoxDataBits.Location = new System.Drawing.Point(205, 13);
+            this.comboBoxDataBits.Name = "comboBoxDataBits";
+            this.comboBoxDataBits.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxDataBits.TabIndex = 2;
+            this.comboBoxDataBits.Text = "DATA BITS";
+            // 
+            // comboBoxParity
+            // 
+            this.comboBoxParity.FormattingEnabled = true;
+            this.comboBoxParity.Location = new System.Drawing.Point(301, 12);
+            this.comboBoxParity.Name = "comboBoxParity";
+            this.comboBoxParity.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxParity.TabIndex = 3;
+            this.comboBoxParity.Text = "PARITY";
+            // 
+            // comboBoxStopBits
+            // 
+            this.comboBoxStopBits.FormattingEnabled = true;
+            this.comboBoxStopBits.Location = new System.Drawing.Point(398, 13);
+            this.comboBoxStopBits.Name = "comboBoxStopBits";
+            this.comboBoxStopBits.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxStopBits.TabIndex = 4;
+            this.comboBoxStopBits.Text = "STOP BITS";
+            // 
+            // comboBoxHandshake
+            // 
+            this.comboBoxHandshake.FormattingEnabled = true;
+            this.comboBoxHandshake.Location = new System.Drawing.Point(495, 13);
+            this.comboBoxHandshake.Name = "comboBoxHandshake";
+            this.comboBoxHandshake.Size = new System.Drawing.Size(90, 21);
+            this.comboBoxHandshake.TabIndex = 5;
+            this.comboBoxHandshake.Text = "HANDSHAKE";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(13, 41);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(90, 23);
+            this.buttonConnect.TabIndex = 6;
+            this.buttonConnect.Text = "CONNECT";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(13, 71);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(186, 20);
+            this.textBoxSend.TabIndex = 7;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(206, 71);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(89, 23);
+            this.buttonSend.TabIndex = 8;
+            this.buttonSend.Text = "SEND";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(495, 70);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(90, 23);
+            this.buttonReset.TabIndex = 9;
+            this.buttonReset.Text = "RESET";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // richTextBoxIncomingData
+            // 
+            this.richTextBoxIncomingData.Location = new System.Drawing.Point(13, 97);
+            this.richTextBoxIncomingData.Name = "richTextBoxIncomingData";
+            this.richTextBoxIncomingData.Size = new System.Drawing.Size(573, 223);
+            this.richTextBoxIncomingData.TabIndex = 10;
+            this.richTextBoxIncomingData.Text = "";
+            // 
+            // labelConnected
+            // 
+            this.labelConnected.AutoSize = true;
+            this.labelConnected.BackColor = System.Drawing.Color.Yellow;
+            this.labelConnected.Location = new System.Drawing.Point(109, 46);
+            this.labelConnected.Name = "labelConnected";
+            this.labelConnected.Size = new System.Drawing.Size(100, 13);
+            this.labelConnected.TabIndex = 11;
+            this.labelConnected.Text = "NOT CONNECTED";
+            // 
+            // DLRadio
+            // 
+            this.DLRadio.AutoSize = true;
+            this.DLRadio.Location = new System.Drawing.Point(449, 74);
+            this.DLRadio.Name = "DLRadio";
+            this.DLRadio.Size = new System.Drawing.Size(39, 17);
+            this.DLRadio.TabIndex = 12;
+            this.DLRadio.TabStop = true;
+            this.DLRadio.Text = "DL";
+            this.DLRadio.UseVisualStyleBackColor = true;
+            this.DLRadio.CheckedChanged += new System.EventHandler(this.DLRadio_CheckedChanged);
+            // 
+            // PHYRadio
+            // 
+            this.PHYRadio.AutoSize = true;
+            this.PHYRadio.Location = new System.Drawing.Point(396, 73);
+            this.PHYRadio.Name = "PHYRadio";
+            this.PHYRadio.Size = new System.Drawing.Size(47, 17);
+            this.PHYRadio.TabIndex = 13;
+            this.PHYRadio.TabStop = true;
+            this.PHYRadio.Text = "PHY";
+            this.PHYRadio.UseVisualStyleBackColor = true;
+            this.PHYRadio.CheckedChanged += new System.EventHandler(this.PHYRadio_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 332);
+            this.Controls.Add(this.PHYRadio);
+            this.Controls.Add(this.DLRadio);
+            this.Controls.Add(this.labelConnected);
+            this.Controls.Add(this.richTextBoxIncomingData);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxHandshake);
+            this.Controls.Add(this.comboBoxStopBits);
+            this.Controls.Add(this.comboBoxParity);
+            this.Controls.Add(this.comboBoxDataBits);
+            this.Controls.Add(this.comboBoxBaudrate);
+            this.Controls.Add(this.comboBoxComPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxComPorts;
+        private System.Windows.Forms.ComboBox comboBoxBaudrate;
+        private System.Windows.Forms.ComboBox comboBoxDataBits;
+        private System.Windows.Forms.ComboBox comboBoxParity;
+        private System.Windows.Forms.ComboBox comboBoxStopBits;
+        private System.Windows.Forms.ComboBox comboBoxHandshake;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.RichTextBox richTextBoxIncomingData;
+        private System.Windows.Forms.Label labelConnected;
+        private System.Windows.Forms.RadioButton DLRadio;
+        private System.Windows.Forms.RadioButton PHYRadio;
+    }
+}
+
---
+++ RS232/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace RS232
+{
+    public enum Send : int
+    {
+        Reset, Phy, Dl
+    }
+
+    public partial class Form1 : Form
+    {
+        private SerialPort comPort = new SerialPort();
+        private bool isConnected = false;
+        public ConcurrentQueue<char> serialDataQueue = new ConcurrentQueue<char>();
+
+        public Boolean statusReceive = true;
+
+        public Form1()
+        {
+            InitializeComponent(); 
+
+            comboBoxComPorts.Items.AddRange(SerialPort.GetPortNames());
+            comboBoxComPorts.SelectedIndex = comboBoxComPorts.Items.Count - 1;
+            
+            comboBoxBaudrate.Items.Add(57600);
+            comboBoxBaudrate.SelectedIndex = comboBoxBaudrate.Items.Count - 1;
+
+            comboBoxDataBits.Items.Add(8);
+            comboBoxDataBits.SelectedIndex = comboBoxDataBits.Items.Count - 1;
+
+            comboBoxParity.Items.Add(Parity.None);
+            comboBoxParity.SelectedIndex = comboBoxParity.Items.Count - 1;
+
+            comboBoxStopBits.Items.Add(StopBits.One);
+            comboBoxStopBits.SelectedIndex = comboBoxStopBits.Items.Count - 1;
+
+            comboBoxHandshake.Items.Add(Handshake.None);
+            comboBoxHandshake.SelectedIndex = comboBoxHandshake.Items.Count - 1;
+
+            comPort.DataReceived += new SerialDataReceivedEventHandler(dataReceived);
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            richTextBoxIncomingData.Clear();
+
+            if (isConnected == false)
+            {
+                comPort.PortName = comboBoxComPorts.Text;
+                comPort.BaudRate = Convert.ToInt32(comboBoxBaudrate.Text);
+                comPort.DataBits = Convert.ToInt32(comboBoxDataBits.Text);
+                comPort.Parity = (Parity)Enum.Parse(typeof(Parity), comboBoxParity.Text);
+                comPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), comboBoxStopBits.Text);
+                comPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), comboBoxHandshake.Text);
+              
+                comPort.ReadTimeout = 200;
+                comPort.WriteTimeout = 200;
+
+                comPort.RtsEnable = false;
+                comPort.DtrEnable = true;
+
+                comPort.Open();
+
+                isConnected = true;
+                labelConnected.BackColor = Color.Green;
+                labelConnected.Text = "CONNECTED";
+                buttonConnect.Text = "DISCONNECT";
+            }
+            else if (isConnected == true)
+            {
+                isConnected = false;
+                labelConnected.Text = "DISCONNECTED";
+                labelConnected.BackColor = Color.Red;
+                buttonConnect.Text = "CONNECT";
+                comPort.Close();
+            }
+        }
+
+        private void dataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            int bytesAvailable = comPort.BytesToRead;
+            char[] recBuf = new char[bytesAvailable];
+
+            try
+            {
+                comPort.Read(recBuf, 0, bytesAvailable);
+
+                for (int index = 0; index < bytesAvailable; index++)
+                {
+                    serialDataQueue.Enqueue(recBuf[index]);
+                }
+
+            }
+            catch (TimeoutException) { }
+
+            readSerialDataQueue();
+
+        }
+
+        private void readSerialDataQueue()
+        {
+            String hexVal = String.Empty;
+
+            int stan = 0;
+            Int32 begin = 0;
+            Int32 length = 0;
+            Int32 cmd = 0;
+            Int32 data = 0;
+            Int32 checksumOne = 0;
+            Int32 checksumTwo = 0;
+
+            List<Int32> dataList = new List<Int32>();
+            List<Int32> checksumList = new List<Int32>();
+
+            Int32 counter = 0;
+
+            try
+            {
+                while (serialDataQueue.TryDequeue(out char ch))
+                {
+                    int val = Convert.ToInt32(ch);
+                    hexVal = String.Format("{0:x2}", val);
+                    
+                    if (stan == 0 && hexVal == "3f") ;
+                    {
+                        statusReceive = true;
+                        continue;
+                    }
+
+                    //poczatek ramki 02 lub 03
+                    if (stan == 0 && (hexVal == "02" || hexVal == "03"))
+                    {
+                        dataList.Clear();
+                        checksumList.Clear();
+                        counter = 0;
+                        begin = val;
+                        this.richTextBoxIncomingData.Text += hexVal;
+
+                        comPort.Write(new byte[] { 0x06 }, 0, 1);
+
+                        stan = 1;
+                        continue;
+                    }
+
+                    //length 0-255
+                    if (stan == 1)
+                    {
+                        length = val;
+                        this.richTextBoxIncomingData.Text += hexVal;
+                        stan = 2;
+
+                        dataList.Add(val);
+
+                        continue;
+                    }
+
+                    //command code 0-FFh
+                    if (stan == 2)
+                    {
+                        cmd = val;
+                        this.richTextBoxIncomingData.Text += hexVal;
+                        stan = 3;
+
+                        dataList.Add(val);
+
+                        continue;
+                    }
+
+                    //data 0-255 Bytes
+                    if (stan == 3)
+                    {
+                        data = val;
+
+                        if (length != 0 && length != counter)
+                        {
+                            this.richTextBoxIncomingData.Text += hexVal;
+                            dataList.Add(val);
+
+                            counter++;
+                        }
+                        else
+                        {
+                            stan = 4;
+                        }
+
+                        continue;
+                    }
+
+                    //checksum 2B (B - Byte)
+                    if (stan == 4) 
+                    {
+                        checksumOne = val;
+                        checksumList.Add(val);
+                        stan = 5;
+
+                        continue;
+                    }
+                    if (stan == 5)
+                    {
+                        checksumTwo = val;
+                        checksumList.Add(val);
+                        stan = 6;
+
+                        continue;
+                    }
+
+                    if(stan == 6)
+                    {
+                        Int32 dataSum = 0;
+                        foreach (Int32 d in dataList)
+                        {
+                            dataSum += d;
+                        }
+                        Int32 checkSum = 0;
+                        foreach (Int32 s in checksumList)
+                        {
+                            checkSum += s;
+                        }
+
+                        if (dataSum == checkSum)
+                        {
+                            stan = 0;
+                            this.richTextBoxIncomingData.Text += Convert.ToString(checksumOne);
+                            this.richTextBoxIncomingData.Text += Convert.ToString(checksumTwo);
+                        }
+                        else
+                        {
+                            stan = 0;
+                            comPort.Write(new byte[] { 0x15 }, 0, 1);
+                        }
+
+                        break;
+                    }
+
+                }
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.StackTrace); }
+        }
+
+        private void SendData(Send send)
+        {
+            comPort.DtrEnable = true;
+            comPort.RtsEnable = true;
+            Thread.Sleep(15);
+
+            if (statusReceive == true)
+            {
+                if (send == Send.Reset)
+                {
+                    comPort.Write(new byte[] { 0x02, 0x00, 0x3c, 0x3c, 0x00 }, 0, 5);
+                }
+                else if (send == Send.Phy)
+                {
+                    comPort.Write(new byte[] { 0x02, 0x02, 0x08, 0x00, 0x00, 0x0a, 0x00 }, 0, 7);
+                }
+                else if (send == Send.Dl)
+                {
+                    comPort.Write(new byte[] { 0x02, 0x02, 0x08, 0x00, 0x01, 0x0b, 0x00 }, 0, 7);
+                }
+
+                statusReceive = false;
+
+            }
+
+            comPort.RtsEnable = false;
+            comPort.DtrEnable = true;
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            SendData(Send.Reset);
+        }
+
+        private void PHYRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (DLRadio.Checked == true)
+            {
+                PHYRadio.Checked = false;
+                SendData(Send.Phy);
+            }
+        }
+
+        private void DLRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (PHYRadio.Checked == true)
+            {
+                DLRadio.Checked = false;
+                SendData(Send.Dl);
+            }
+        }
+    }
+
+}
